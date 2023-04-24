17631bd050d4e8ccb1f63f7b45f182bcfa615f30
Razmeshala87 <Razmeshala87@gmail.com>
2023-04-24 14:23:21 +0200

    Commit My Home Work

--- домашнее задание 16.py
+++ домашнее задание 16.py
@@ -6,22 +6,17 @@ while(True):
         break
     else:
         x1 = input("Вид питомца: ")
-        x2 = input("Возраст питомца: ")
+        x2 = int(input("Возраст питомца: "))
+        year_case = ""
+        if x2 % 10 == 1 and x2 != 11 and x2 % 100 != 11:
+            year_case = "год"
+        elif 1 < x2 % 10 <= 4 and x2 != 12 and x2 != 13 and x2 != 14:
+            year_case = "года"
+        else:
+            year_case = "лет"
         x3 = input("Имя владельца: ")
 
-    pets2 = {"Вид питомца:":x1, "Возраст питомца:":x2, "Имя владельца:":x3 }
+    pets2 = {"Вид питомца: " :x1, "Возраст питомца: " :x2, "Имя владельца: " :x3 }
     pets[x] = pets2
-        
-for key, value in pets.items():
-    view = value["Вид питомца: "]
-    age = value["Возраст питомца: "]
-    name = value["Имя владельца: "]
-    year_case = ""
-    if age % 10 == 1 and age != 11 and age % 100 != 11:
-        year_case = "год"
-    elif 1 < age % 10 <= 4 and age != 12 and age != 13 and age != 14:
-        year_case = "года"
-    else:
-        year_case = "лет"
-    print("Это", view, "по кличке", key, ". Возраст питомца:", age, year_case, "Имя владельца: ", name)
+    print("Это", pets2["Вид питомца: "], "по кличке", pets.keys(), ". Возраст питомца:", pets2["Возраст питомца: "], year_case, "Имя владельца: ", pets2["Имя владельца: "])
 
